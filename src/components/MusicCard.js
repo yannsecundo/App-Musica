874c041ad9d7a1cd7055e5a697956e91c8bed6a9
Yann Secundo <yannsecundo34@gmail.com>
2022-07-24 23:42:31 -0400

    requsiitos 7,8,9 e 10 sobre musica e finalizando os requisitos obrigatórios

---
+++ src/components/MusicCard.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+
+export default class MusicCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { favorite: props.favoriteSongs };
+  }
+
+  onHandleChange = ({ target }) => {
+    const { checked } = target;
+    this.setState({ loading: true, favorite: checked }, async () => {
+      const { trackName, previewUrl, trackId, updateFavoriteSongs } = this.props;
+      const { favorite } = this.state;
+      if (favorite) {
+        await addSong({ trackId, trackName, previewUrl });
+      } else {
+        await removeSong({ trackId, trackName, previewUrl });
+      }
+      updateFavoriteSongs();
+      this.setState({ loading: false });
+    });
+  };
+
+  render() {
+    const { trackName, previewUrl, trackId } = this.props;
+    const { favorite, loading } = this.state;
+    return (
+      <div>
+        <p>{trackName}</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          <code>audio</code>
+        </audio>
+        {loading ? (
+          <Loading />
+        ) : (
+          <label htmlFor="favorite">
+            Favorita
+            <input
+              data-testid={ `checkbox-music-${trackId}` }
+              type="checkbox"
+              name="favorite"
+              id="favorite"
+              checked={ favorite }
+              onChange={ this.onHandleChange }
+            />
+          </label>
+        )}
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  updateFavoriteSongs: PropTypes.func.isRequired,
+  favoriteSongs: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      albumName: '',
+      loading: false,
+      musicResults: [],
+      favoriteSongs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  fetchApi = () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    this.setState({ loading: true }, async () => {
+      const results = await getMusics(id);
+      const favorite = await getFavoriteSongs();
+      this.setState({
+        artistName: results[0].artistName,
+        albumName: results[0].collectionName,
+        musicResults: results,
+        favoriteSongs: favorite,
+        loading: false,
+      });
+    });
+  };
+
   render() {
+    const {
+      musicResults,
+      artistName,
+      albumName,
+      loading,
+      favoriteSongs } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        album
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <p data-testid="artist-name">{artistName}</p>
+            <p data-testid="album-name">{albumName}</p>
+            {musicResults
+              .filter((music) => music.kind === 'song')
+              .map((music) => (
+                <MusicCard
+                  key={ music.trackId }
+                  trackId={ music.trackId }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  favoriteSongs={ favoriteSongs.some(
+                    ({ trackId: id }) => id === music.trackId,
+                  ) }
+                  updateFavoriteSongs={ () => {} }
+                />
+              ))}
+          </div>
+        )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
