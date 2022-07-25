0786dd4c2ad3b0224f97fd2034257c696ceec4dc
Yann Secundo <yannsecundo34@gmail.com>
2022-07-24 23:19:30 -0400

    requisito 4,5 e 6 tudo sobre a page search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,12 +1,93 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicList from '../components/MusicList';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: '',
+      isButtonDisabled: true,
+      loading: false,
+      artistSearch: '',
+      searchResults: [],
+    };
+  }
+
+  buttonCondition = () => {
+    const { inputSearch } = this.state;
+    const inputMinLength = 3;
+    this.setState({ isButtonDisabled: inputSearch.length < inputMinLength });
+  };
+
+  onHandleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ inputSearch: value }, this.buttonCondition);
+  };
+
+  fetchSearch = () => {};
+
+  onHandleClick = () => {
+    const { inputSearch } = this.state;
+    this.setState(
+      {
+        loading: true,
+        artistSearch: inputSearch,
+        inputSearch: '',
+        isButtonDisabled: true,
+      },
+      async () => {
+        this.setState({
+          searchResults: await searchAlbumsAPI(inputSearch),
+          loading: false,
+        });
+      },
+    );
+  };
+
   render() {
+    const {
+      inputSearch,
+      isButtonDisabled,
+      artistSearch,
+      searchResults,
+      loading,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        pesquisar
+        <h3>Search</h3>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          name="inputSearch"
+          value={ inputSearch }
+          onChange={ this.onHandleChange }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ isButtonDisabled }
+          onClick={ this.onHandleClick }
+        >
+          Pesquisar
+        </button>
+        {loading ? (
+          <Loading />
+        ) : (
+          artistSearch && (
+            <div>
+              <span>
+                Resultado de álbuns de:
+                {' '}
+                {artistSearch}
+              </span>
+              <MusicList searchResults={ searchResults } />
+            </div>
+          )
+        )}
       </div>
     );
   }
